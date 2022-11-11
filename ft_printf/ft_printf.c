fa29cd43707c73b7aa0d399ab11fd9f59ec4abd9
TristanMlct <tristanmlct@gmail.com>
2022-11-12 00:34:22 +0100

    add libft and makefile (not working)

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,38 +6,35 @@
 /*   By: tmilcent <tmilcent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 22:14:46 by tmilcent          #+#    #+#             */
-/*   Updated: 2022/11/11 12:50:36 by tmilcent         ###   ########.fr       */
+/*   Updated: 2022/11/12 00:19:15 by tmilcent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "includes/libft.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int	print_char(va_list args)
+int	print_char(int c)
 {
-	ft_putchar_fd(va_arg(args, int), 1);
+	ft_putchar_fd(c, 1);
 	return (1);
 }
 
-int	print_string(va_list args)
+int	print_string(char *str)
 {
-	char	*to_print;
-
-	to_print = va_arg(args, char *);
-	ft_putstr_fd(to_print, 1);
-	return (ft_strlen(to_print));
+	ft_putstr_fd(str, 1);
+	return (ft_strlen(str));
 }
 
-int	print_ptr_hex(va_list args)
+int	print_ptr_hex(void *ptr)
 {
 	unsigned long long	arg;
 	char				to_print[13];
 	int					c;
 
 	to_print[12] = '\0';
-	arg = (unsigned long long)va_arg(args, void *);
+	arg = (unsigned long long)ptr;
 	c = 0;
 	while (arg > 0)
 	{
@@ -58,12 +55,12 @@ int	print_ptr_hex(va_list args)
 	return (14);
 }
 
-int	print_int(va_list args)
+int	print_int(int nb)
 {
 	int		r;
 	char	*str;
 
-	str = ft_itoa(va_arg(args, int));
+	str = ft_itoa(nb);
 	ft_putstr_fd(str, 1);
 	r = ft_strlen(str);
 	return (r); 
@@ -72,21 +69,21 @@ int	print_int(va_list args)
 int	print_format(char c, va_list args)
 {
 	if (c == 'c')
-		return (print_char(args));
+		return (print_char(va_arg(args, int)));
 	else if (c == 's')
-		return (print_string(args));
+		return (print_string(va_arg(args, char *)));
 	else if (c == 'p')
-		return (print_ptr_hex(args));
+		return (print_ptr_hex(va_arg(args, void *)));
 	else if (c == 'd' || c == 'i')
-		return (print_int(args));
+		return (print_int(va_arg(args, int)));
 	// else if (c == 'u')
 	// 	return (print_int(args));
 	// else if (c == 'x')
 	// 	return (print_int(args));
 	// else if (c == 'X')
-	// 	return (print_int(args));
-	// else if (c == '%')
-	// 	return (print_int(args));
+		// return (print_int(args));
+	else if (c == '%')
+		return (print_char('%'));
 	// else
 	// {
 	// 	error
@@ -122,6 +119,28 @@ int	ft_printf(const char *str, ...)
 int	main(void)
 {
 	char *ptr = "non";
-	printf("%d\n", ft_printf("%d %i %d \n", 1, 25, 123));
-	printf("%d\n", printf("%d %i %d \n", 1, 25, 123));
+	// c
+	printf("%d\n", ft_printf("%c %c \n", 'a', '\n'));
+	printf("%d\n", printf("%c %c \n", 'a', '\n'));
+	// s
+	printf("%d\n", ft_printf("%s %s \n", "test", "aaa\n"));
+	printf("%d\n", printf("%s %s \n", "test", "aaa\n"));
+	// p
+	printf("%d\n", ft_printf("%p \n", ptr));
+	printf("%d\n", printf("%p \n", ptr));
+	// d && i
+	printf("%d\n", ft_printf("%d %i %d \n", 2147483647, 25, 123));
+	printf("%d\n", printf("%d %i %d \n", 2147483647, 25, 123));
+	// u
+	// printf("%d\n", ft_printf("%d %i %d \n", 1, 25, 123));
+	// printf("%d\n", printf("%u \n", 2147483648));
+	// x
+	// printf("%d\n", ft_printf("%d %i %d \n", 1, 25, 123));
+	printf("%d\n", printf("%x \n", 494967295));
+	// X
+	// printf("%d\n", ft_printf("%d %i %d \n", 1, 25, 123));
+	printf("%d\n", printf("%X \n", 494967295));
+	// %
+	printf("%d\n", ft_printf("%% \n"));
+	printf("%d\n", printf("%% \n"));
 }
